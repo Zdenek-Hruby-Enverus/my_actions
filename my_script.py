3366358684dff872e5cf34a22b10d007fc694778
Zdenek Hruby <zdenek.hruby@drillinginfo.com>
2023-09-11 17:35:35 +0200

    add argparse to python script

--- my_script.py
+++ my_script.py
@@ -1,7 +1,15 @@
 import random
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("--mu", type=int, default=0)
+parser.add_argument("--sigma", type=int, default=1)
+
+args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    rn = random.gauss(mu=0, sigma=1)
+    
+    rn = random.gauss(mu=args.mu, sigma=args.sigma)
 
     print(f"My random number is: {rn}")
