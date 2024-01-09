12bcbc53007346e59719e384c11f54a6a2df5db0
Zdenek Hruby <144209297+Zdenek-Hruby-Enverus@users.noreply.github.com>
2024-01-09 11:58:16 +0100

    test log exception in gha

--- scripts/greetings.py
+++ scripts/greetings.py
@@ -6,6 +6,13 @@ parser.add_argument("--name", type=str, required=True)
 parser.add_argument("--place", type=str, required=True)
 
 my_args = parser.parse_args()
+a = 1
+b = 0
 
 if __name__ == "__main__":
+    try:
+        a/b
+    except ZeroDivisionError as e:
+        raise ZeroDivisionError("skldfjslfjlsdfj alksdjlkdsjf ejrwiojr")
+
     print(f"Hello {my_args.name} from {my_args.place}.")
